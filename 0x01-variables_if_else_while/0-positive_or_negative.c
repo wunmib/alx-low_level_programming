b8c059d74770732460137c1eaca3335b5fbdc598
wunmib <bonojowunmi@gmail.com>
2022-09-10 14:27:45 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,15 +9,16 @@
 */
 int main main (void)
 {
-int n;
-srand(time(0);
-n=rand() - RAND_MAX / 2;
-if (n < 0)
-{
-  printf("%d is negative\n",n)
-} else if (n == 0)
-}
-printf("%d is zero\n",n);
-}
-return (0);
+	int n;
+	srand(time(0);
+	n=rand() - RAND_MAX / 2;
+	if (n < 0)
+	{	
+  		printf("%d is negative\n",n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n",n);
+	}	
+	return (0);
 }
