3274352086419c7b3edb358e3365e989c987f2ff
wunmib <bonojowunmi@gmail.com>
2022-09-10 13:53:22 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,24 +3,21 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Return:always return 0
- */
-int main (void)
+*main - entry point
+*Return: always return 0
+*function main - find the last digit of n
+*/
+int main main (void)
 {
-	int n;
-	srand (time(0));
-	n = rand() - RAND_MAX /2;
-	if (n < 0)
-	{
-		ptintf("%d is negative\n", n)
-	} else if (n == Q)
-	{
-		printf("%d is zero\n", n);
-	} else
-	{
-		printf("%d is positive\n", n);
-	}
-	return (0);
+int n;
+srand(time(0);
+n=rand() - RAND_MAX / 2;
+if (n < 0)
+{
+  printf("%d is negative\n",n)
+} else if (n == 0)
+}
+printf("%d is zero\n",n);
+}
+return (0);
 }
