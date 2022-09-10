fa10ebc24e169d64a809619500665382071d845e
wunmib <bonojowunmi@gmail.com>
2022-09-10 14:56:01 -0700

    negativecommit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
-*main - entry point
+*Description: main - entry point
 *Return: always return 0
 *function main - find the last digit of n
 */
 int main main (void)
 {
 	int n;
-	srand(time(0);
-	n=rand() - RAND_MAX / 2;
-	if (n < 0)
-	{	
-  		printf("%d is negative\n",n);
-	}
+
+	srand(time(0) );
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n",n);
 	else if (n == 0)
-	{
 		printf("%d is zero\n",n);
-	}	
+	else 
+		printf("%d is negative\n" , n);
 	return (0);
 }
