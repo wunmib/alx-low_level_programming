67157c93d97616b91615338d2c7b2670e7296751
wunmib <bonojowunmi@gmail.com>
2022-09-11 10:40:39 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Description main - print out a string to stdout.
+ * Return: 0 if successfull.
+ */
+int main(void)
+{
+	char low;
+	
+	for (low = 'a'; low <= 'z'; low++) 
+		putchar(low);
+	for (low = 'A'; low <= 'z'; low++)
+		putchar(low);
+	printf("\n");
+	
+	return (0);
+}
