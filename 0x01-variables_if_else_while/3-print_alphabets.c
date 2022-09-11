e820fa9091f74d9a7e2122644f64f386e2a32cde
wunmib <bonojowunmi@gmail.com>
2022-09-11 12:51:53 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,12 @@
 int main(void)
 {
 	char low;
-	for ("low = 'a'; low" <= 'z'; low++) 
+
+	for (low = 'a'; low <= 'z'; low++) 
 		putchar(low);
 	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
 	printf("\n");
+
 	return (0);
 }
