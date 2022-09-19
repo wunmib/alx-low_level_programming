dc0599bf4ae2526646e13629079a771b754da67a
wunmib <bonojowunmi@gmail.com>
2022-09-19 13:55:09 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,10 +2,12 @@
 #include "main.h"
 
 /**
- * _puts - function that print a string, followed by new line
+* _puts - function that print a string, followed by new line
 * @str: pointer that point the string
 * Return: nothing
 */
+
+void _puts(char *str)
 {
 	int k = 0;
 
