1c0953662fc13be521335a6c63fb19f011c0b172
wunmib <bonojowunmi@gmail.com>
2022-09-28 13:38:00 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
+
+chmod u+x 0-puts_recursion.c
+git add .
+git commit -m ‘recursion’
+git push
